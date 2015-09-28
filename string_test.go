3348c72c7bd7eca902795b1f6ad08124bafb4016
Vincent Demeester <vincent@sbr.pm>
2015-09-28 17:20:37 +0200

    Add more string checkers (and refactor)
    - HasSuffix
    - HasPrefix
    - ContainsAny

--- string_test.go
+++ string_test.go
@@ -11,7 +11,7 @@ func init() {
 type StringCheckerS struct{}
 
 func (s *StringCheckerS) TestContains(c *check.C) {
-	testInfo(c, Contains, "Contains", []string{"value", "substring"})
+	testInfo(c, Contains, "Contains", []string{"obtained", "substring"})
 
 	testCheck(c, Contains, true, "", "abcd", "bc")
 	testCheck(c, Contains, false, "", "abcd", "efg")
@@ -19,6 +19,46 @@ func (s *StringCheckerS) TestContains(c *check.C) {
 	testCheck(c, Contains, true, "", "abcd", "")
 	testCheck(c, Contains, true, "", "", "")
 
-	testCheck(c, Contains, false, "Obtained value is not a string and has no .String()", 12, "1")
-	testCheck(c, Contains, false, "Substring must be a string", "", 1)
+	testCheck(c, Contains, false, "obtained value is not a string and has no .String().", 12, "1")
+	testCheck(c, Contains, false, "substring value must be a string.", "", 1)
+}
+
+func (s *StringCheckerS) TestContainsAny(c *check.C) {
+	testInfo(c, ContainsAny, "ContainsAny", []string{"obtained", "chars"})
+
+	testCheck(c, ContainsAny, true, "", "abcd", "b")
+	testCheck(c, ContainsAny, true, "", "abcd", "b & c")
+	testCheck(c, ContainsAny, false, "", "abcd", "e")
+	testCheck(c, ContainsAny, false, "", "", "bc")
+	testCheck(c, ContainsAny, false, "", "abcd", "")
+	testCheck(c, ContainsAny, false, "", "", "")
+
+	testCheck(c, ContainsAny, false, "obtained value is not a string and has no .String().", 12, "1")
+	testCheck(c, ContainsAny, false, "substring value must be a string.", "", 1)
+}
+
+func (s *StringCheckerS) TestHasPrefix(c *check.C) {
+	testInfo(c, HasPrefix, "HasPrefix", []string{"obtained", "prefix"})
+
+	testCheck(c, HasPrefix, true, "", "abcd", "ab")
+	testCheck(c, HasPrefix, false, "", "abcd", "efg")
+	testCheck(c, HasPrefix, false, "", "", "bc")
+	testCheck(c, HasPrefix, true, "", "abcd", "")
+	testCheck(c, HasPrefix, true, "", "", "")
+
+	testCheck(c, HasPrefix, false, "obtained value is not a string and has no .String().", 12, "1")
+	testCheck(c, HasPrefix, false, "substring value must be a string.", "", 1)
+}
+
+func (s *StringCheckerS) TestHasSuffix(c *check.C) {
+	testInfo(c, HasSuffix, "HasSuffix", []string{"obtained", "suffix"})
+
+	testCheck(c, HasSuffix, true, "", "abcd", "cd")
+	testCheck(c, HasSuffix, false, "", "abcd", "efg")
+	testCheck(c, HasSuffix, false, "", "", "bc")
+	testCheck(c, HasSuffix, true, "", "abcd", "")
+	testCheck(c, HasSuffix, true, "", "", "")
+
+	testCheck(c, HasSuffix, false, "obtained value is not a string and has no .String().", 12, "1")
+	testCheck(c, HasSuffix, false, "substring value must be a string.", "", 1)
 }
