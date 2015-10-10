3f4a948f9b1b3916530fc762805b4ceea6e3b40a
Vincent Demeester <vincent@sbr.pm>
2015-10-10 17:00:26 +0200

    Add GreaterThan and LessThan checkers

--- common.go
+++ common.go
@@ -76,3 +76,119 @@ func equalerEquals(obtained, expected interface{}) (bool, string) {
 	}
 	return obtainedEqualer.Equal(expectedEqualer), ""
 }
+
+// GreaterThan checker verifies the obtained value is greater than the specified one.
+// It's is smart in a wait that it supports several *types* (built-in, time.Time)
+//
+//    c.Assert(myTime, GreaterThan, aTime, check.Commentf("bouuuhh"))
+//    c.Assert(myInt, GreaterThan, 2, check.Commentf("bouuuhh"))
+//
+var GreaterThan check.Checker = &greaterThanChecker{
+	&check.CheckerInfo{
+		Name:   "GreaterThan",
+		Params: []string{"obtained", "expected"},
+	},
+}
+
+type greaterThanChecker struct {
+	*check.CheckerInfo
+}
+
+func (checker *greaterThanChecker) Check(params []interface{}, names []string) (bool, string) {
+	return greaterThan(params[0], params[1])
+}
+
+func greaterThan(obtained, expected interface{}) (bool, string) {
+	if _, ok := obtained.(time.Time); ok {
+		return isAfter(obtained, expected)
+	}
+	if reflect.TypeOf(obtained) != reflect.TypeOf(expected) {
+		return false, "obtained value and expected value have not the same type."
+	}
+	switch v := obtained.(type) {
+	case float32:
+		return v > expected.(float32), ""
+	case float64:
+		return v > expected.(float64), ""
+	case int:
+		return v > expected.(int), ""
+	case int8:
+		return v > expected.(int8), ""
+	case int16:
+		return v > expected.(int16), ""
+	case int32:
+		return v > expected.(int32), ""
+	case int64:
+		return v > expected.(int64), ""
+	case uint:
+		return v > expected.(uint), ""
+	case uint8:
+		return v > expected.(uint8), ""
+	case uint16:
+		return v > expected.(uint16), ""
+	case uint32:
+		return v > expected.(uint32), ""
+	case uint64:
+		return v > expected.(uint64), ""
+	default:
+		return false, "obtained value type not supported."
+	}
+}
+
+// LessThan checker verifies the obtained value is greater than the specified one.
+// It's is smart in a wait that it supports several *types* (built-in, time.Time)
+//
+//    c.Assert(myTime, LessThan, aTime, check.Commentf("bouuuhh"))
+//    c.Assert(myInt, LessThan, 2, check.Commentf("bouuuhh"))
+//
+var LessThan check.Checker = &lessThanChecker{
+	&check.CheckerInfo{
+		Name:   "LessThan",
+		Params: []string{"obtained", "expected"},
+	},
+}
+
+type lessThanChecker struct {
+	*check.CheckerInfo
+}
+
+func (checker *lessThanChecker) Check(params []interface{}, names []string) (bool, string) {
+	return lessThan(params[0], params[1])
+}
+
+func lessThan(obtained, expected interface{}) (bool, string) {
+	if _, ok := obtained.(time.Time); ok {
+		return isBefore(obtained, expected)
+	}
+	if reflect.TypeOf(obtained) != reflect.TypeOf(expected) {
+		return false, "obtained value and expected value have not the same type."
+	}
+	switch v := obtained.(type) {
+	case float32:
+		return v < expected.(float32), ""
+	case float64:
+		return v < expected.(float64), ""
+	case int:
+		return v < expected.(int), ""
+	case int8:
+		return v < expected.(int8), ""
+	case int16:
+		return v < expected.(int16), ""
+	case int32:
+		return v < expected.(int32), ""
+	case int64:
+		return v < expected.(int64), ""
+	case uint:
+		return v < expected.(uint), ""
+	case uint8:
+		return v < expected.(uint8), ""
+	case uint16:
+		return v < expected.(uint16), ""
+	case uint32:
+		return v < expected.(uint32), ""
+	case uint64:
+		return v < expected.(uint64), ""
+	default:
+		return false, "obtained value type not supported."
+	}
+}
